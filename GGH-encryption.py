0b25cfe0d859201c051e0e4cdbc0ce3de2675315
Programmer707707 <toxirovsherzod707@gmail.com>
2025-05-17 21:02:47 +0200

    first commit

---
+++ GGH-encryption.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+def build(n):
+    while True:
+        mat = np.random.randint(-3, 4, size=(n, n))
+        if abs(round(np.linalg.det(mat))) == 1:
+            return mat
+
+def prep_keys(n=3):
+    B = np.array([[3, 0, 0], [0, 5, 0], [0, 0, 7]], dtype=int)   #np.eye(n, dtype=int)  #[[1, 0, 0], [0, 1, 0], [0, 0, 1]]        
+    B_inv = np.linalg.inv(B)
+    U = build(n)              
+    U_inv = np.linalg.inv(U)
+    B_pub = U @ B                    
+    return B, B_inv, U, U_inv, B_pub
+
+def encrypt(B_pub, m, error_range=1):
+    e = np.random.randint(-error_range, error_range+1, size=B_pub.shape[0])
+    c = m @ B_pub + e                    
+    return c
+
+def decrypt(c, B_inv, U_inv):
+    y = c @ B_inv                        
+    y_rounded = np.round(y)             
+    m = y_rounded @ U_inv               
+    return np.round(m).astype(int)
+
+def main():
+    B, B_inv, U, U_inv, B_pub = prep_keys(n=3)
+
+    print("Secret Basis B:\n", B)
+    print("Public Basis B' = U × B:\n", B_pub)
+
+    print("Uni-modular Matrix: \n", U)
+    
+    m = np.array([12, -11, 10])           
+    print("\nOriginal Message m:\n", m)
+
+    c = encrypt(B_pub, m, error_range=1)
+    print("Encrypted Ciphertext c:\n", c)
+
+    m_recovered = decrypt(c, B_inv, U_inv)
+    print("Recovered Message m:\n", m_recovered)
+
+if __name__ == "__main__":
+    main()
+
+
+# print(np.array([[1,0,0], [0,1,0], [0,0,1]]).dtype)
+# print(np.eye(3, dtype=int).dtype)
+
+# A = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]], dtype=int)
+# B = np.eye(3, dtype=int)
+
+# print("Same values:", np.array_equal(A, B))  
+# print("Same memory layout:", A.flags['C_CONTIGUOUS'], B.flags['C_CONTIGUOUS'])
